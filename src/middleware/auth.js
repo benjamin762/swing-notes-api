b954849529b59e7f2aa5fe93b9f55302d0ab4c35
Benjamin Stenlund <benjamin.stenlund@chasacademy.se>
2025-06-16 16:09:44 +0200

    Authorization WIP

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,12 +3,12 @@ import bcrypt from 'bcrypt'
 import { JWT_SECRET } from '../utils/config.js';
 
 function authenticate (req, res, next) {
-    const token = req.headers.authorize;
-    if(!token){res.status(400).json({error:'Authorisation missing.'})}
+    const token = req.headers.authorization?.split(' ')[1];
+    if(!token){return res.status(400).json({error:'Authorisation token missing.'})}
 
-    // const user = jwt.verify(token, JWT_SECRET)
-
-    // res.locals.user = user
+    const user = jwt.verify(token, JWT_SECRET)
+console.log(user)
+    res.locals.user = user
 
     // try catch
 
