d0e3711de7075348f14014d9fa3f497e07efb09e
Benjamin Stenlund <benjamin.stenlund@chasacademy.se>
2025-06-16 13:57:21 +0200

    Comment

--- src/utils/config.js
+++ src/utils/config.js
@@ -10,12 +10,8 @@ const DATABASE = {
     password: process.env.PGPASSWORD,
     port: process.env.PGPORT ?? '5432',
 }
+// Check if any value in DATABASE is undefined.
 if (Object.values(DATABASE).some(value => !value)) {throw new Error('Database details missing, see readme for .env')}
-// for (let key in DATABASE) {
-//     if (!DATABASE[key]) {
-//         throw new Error('Database ')
-//     }
-// }
 
 const JWT_SECRET = process.env.JWT_SECRET
 if (!JWT_SECRET) {throw new Error('JWT-secret missing, add to .env')}
