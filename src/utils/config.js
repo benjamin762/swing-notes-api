18cb2c83be0aef257de8479d94beb9d2f29a053d
Benjamin Stenlund <benjamin.stenlund@chasacademy.se>
2025-06-16 13:56:34 +0200

    Improve config error

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,13 +4,18 @@ const PORT = process.env.PORT ?? 3000
 
 
 const DATABASE = {
-    user: process.env.PGUSER ?? 'postgres',
+    user: process.env.PGUSER,
     host: process.env.PGHOST ?? 'localhost',
     database: process.env.PGDATABASE ?? 'swing-notes',
     password: process.env.PGPASSWORD,
     port: process.env.PGPORT ?? '5432',
 }
-if (!DATABASE.password) {throw new Error('Database urlgfhjfghfhjgf TODO missing, add to .env')}
+if (Object.values(DATABASE).some(value => !value)) {throw new Error('Database details missing, see readme for .env')}
+// for (let key in DATABASE) {
+//     if (!DATABASE[key]) {
+//         throw new Error('Database ')
+//     }
+// }
 
 const JWT_SECRET = process.env.JWT_SECRET
 if (!JWT_SECRET) {throw new Error('JWT-secret missing, add to .env')}
